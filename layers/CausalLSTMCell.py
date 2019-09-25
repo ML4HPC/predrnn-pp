1a2358d6d20277293dd49217e13ab98cb973fcfa
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2019-09-25 16:45:34 -0400

    Write pytorch version of CausalLSTMCell

--- layers/CausalLSTMCell.py
+++ layers/CausalLSTMCell.py
@@ -1,136 +1,144 @@
-__author__ = 'yunbo'
+import torch
+from torch import nn
 
-import tensorflow as tf
-from layers.TensorLayerNorm import tensor_layer_norm
 
-class CausalLSTMCell():
-    def __init__(self, layer_name, filter_size, num_hidden_in, num_hidden_out,
-                 seq_shape, forget_bias=1.0, tln=False, initializer=0.001):
-        """Initialize the Causal LSTM cell.
-        Args:
-            layer_name: layer names for different lstm layers.
-            filter_size: int tuple thats the height and width of the filter.
-            num_hidden_in: number of units for input tensor.
-            num_hidden_out: number of units for output tensor.
-            seq_shape: shape of a sequence.
-            forget_bias: float, The bias added to forget gates.
-            tln: whether to apply tensor layer normalization
+class CausalLSTM(nn.Module):
+    def __init__(self, filter_size, num_hidden_in, num_hidden_out,
+                 seq_shape, forget_bias=1.0, layer_norm=False):
         """
-        self.layer_name = layer_name
+        Initialize the Causal LSTM cell.
+
+        Parameters
+        ==========
+        filter_size:
+            int tuple thats the height and width of the filter.
+        num_hidden_in:
+            number of units for input tensor.
+        num_hidden_out:
+            number of units for output tensor.
+        seq_shape:
+            shape of a sequence.
+        forget_bias: float
+            The bias added to forget gates.
+        layer_norm:
+            whether to apply tensor layer normalization
+        """
         self.filter_size = filter_size
         self.num_hidden_in = num_hidden_in
-        self.num_hidden = num_hidden_out
+        self.num_hidden_out = num_hidden_out
+
         self.batch = seq_shape[0]
         self.height = seq_shape[2]
         self.width = seq_shape[3]
-        self.layer_norm = tln
+
+        self.layer_norm = layer_norm
         self._forget_bias = forget_bias
-        self.initializer = tf.random_uniform_initializer(-initializer,initializer)
 
-    def init_state(self):
-        return tf.zeros([self.batch, self.height, self.width, self.num_hidden],
-                        dtype=tf.float32)
+        self.conv_h = \
+            nn.Conv2d(num_hidden_out, num_hidden_out * 4, filter_size,
+                      stride=1, padding=1, padding_mod='replicate')
+        self.conv_c = \
+            nn.Conv2d(num_hidden_out, num_hidden_out * 3, filter_size,
+                      stride=1, padding=1, padding_mod='replicate')
+        self.conv_m = \
+            nn.Conv2d(num_hidden_in, num_hidden_out * 3, filter_size,
+                      stride=1, padding=1, padding_mod='replicate')
+        self.conv_x = \
+            nn.Conv2d(num_hidden_in, num_hidden_out * 7, filter_size,
+                      stride=1, padding=1, padding_mod='replicate')
 
-    def __call__(self, x, h, c, m):
+        self.conv_c2 = \
+            nn.Conv2d(num_hidden_out, num_hidden_out * 4, filter_size,
+                      stride=1, padding=1, padding_mod='replicate')
+        self.conv_o = \
+            nn.Conv2d(num_hidden_out, num_hidden_out, filter_size,
+                      stride=1, padding=1, padding_mod='replicate')
+        self.conv_h2 = \
+            nn.Conv2d(num_hidden_out, num_hidden_out, 1,
+                      stride=1, padding=1, padding_mod='replicate')
+
+        self.ln_h = nn.LayerNorm(num_hidden_out * 4, elementwise_affine=True)
+        self.ln_c = nn.LayerNorm(num_hidden_out * 3, elementwise_affine=True)
+        self.ln_m = nn.LayerNorm(num_hidden_out * 3, elementwise_affine=True)
+        self.ln_x = nn.LayerNorm(num_hidden_out * 7, elementwise_affine=True)
+        self.ln_c2 = nn.LayerNorm(num_hidden_out * 4, elementwise_affine=True)
+        self.ln_o = nn.LayerNorm(num_hidden_out, elementwise_affine=True)
+
+    def forward(self, x, h=None, c=None, m=None):
+        def run_layer_norm(x, ln):
+            return ln(x.permute(0, 2, 3, 1)).permute(0, -1, 1, 2)
+
         if h is None:
-            h = tf.zeros([self.batch, self.height, self.width,
-                          self.num_hidden],
-                         dtype=tf.float32)
+            dim_h = [self.batch, self.num_hidden_out, self.height, self.width]
+            h = torch.zeros(dim_h, dtype=torch.float32)
+
         if c is None:
-            c = tf.zeros([self.batch, self.height, self.width,
-                          self.num_hidden],
-                         dtype=tf.float32)
+            dim_c = [self.batch, self.num_hidden_out, self.height, self.width]
+            c = torch.zeros(dim_c, dtype=torch.float32)
+
         if m is None:
-            m = tf.zeros([self.batch, self.height, self.width,
-                          self.num_hidden_in],
-                         dtype=tf.float32)
+            dim_m = [self.batch, self.num_hidden_in, self.height, self.width]
+            m = torch.zeros(dim_m, dtype=torch.float32)
 
-        with tf.variable_scope(self.layer_name):
-            h_cc = tf.layers.conv2d(
-                h, self.num_hidden*4,
-                self.filter_size, 1, padding='same',
-                kernel_initializer=self.initializer,
-                name='temporal_state_transition')
-            c_cc = tf.layers.conv2d(
-                c, self.num_hidden*3,
-                self.filter_size, 1, padding='same',
-                kernel_initializer=self.initializer,
-                name='temporal_memory_transition')
-            m_cc = tf.layers.conv2d(
-                m, self.num_hidden*3,
-                self.filter_size, 1, padding='same',
-                kernel_initializer=self.initializer,
-                name='spatial_memory_transition')
+        h_cc = self.conv_h(h)
+        c_cc = self.conv_c(c)
+        m_cc = self.conv_m(m)
+
+        if self.layer_norm:
+            h_cc = run_layer_norm(h_cc, self.ln_h)
+            c_cc = run_layer_norm(c_cc, self.ln_c)
+            m_cc = run_layer_norm(m_cc, self.ln_m)
+
+        i_h, g_h, f_h, o_h = torch.split(h_cc, 4, 1)
+        i_c, g_c, f_c = torch.split(c_cc, 3, 1)
+        i_m, f_m, m_m = torch.split(m_cc, 3, 1)
+
+        if x is None:
+            i = torch.sigmoid(i_h + i_c)
+            f = torch.sigmoid(f_h + f_c + self._forget_bias)
+            g = torch.tanh(g_h + g_c)
+        else:
+            x_cc = self.conv_x(x)
             if self.layer_norm:
-                h_cc = tensor_layer_norm(h_cc, 'h2c')
-                c_cc = tensor_layer_norm(c_cc, 'c2c')
-                m_cc = tensor_layer_norm(m_cc, 'm2m')
+                x_cc = run_layer_norm(x_cc, self.ln_x)
 
-            i_h, g_h, f_h, o_h = tf.split(h_cc, 4, 3)
-            i_c, g_c, f_c = tf.split(c_cc, 3, 3)
-            i_m, f_m, m_m = tf.split(m_cc, 3, 3)
+            i_x, g_x, f_x, o_x, i_x_, g_x_, f_x_ = torch.split(x_cc, 7, 1)
 
-            if x is None:
-                i = tf.sigmoid(i_h + i_c)
-                f = tf.sigmoid(f_h + f_c + self._forget_bias)
-                g = tf.tanh(g_h + g_c)
-            else:
-                x_cc = tf.layers.conv2d(
-                    x, self.num_hidden*7,
-                    self.filter_size, 1, padding='same',
-                    kernel_initializer=self.initializer,
-                    name='input_to_state')
-                if self.layer_norm:
-                    x_cc = tensor_layer_norm(x_cc, 'x2c')
+            i = torch.sigmoid(i_x + i_h + i_c)
+            f = torch.sigmoid(f_x + f_h + f_c + self._forget_bias)
+            g = torch.tanh(g_x + g_h + g_c)
 
-                i_x, g_x, f_x, o_x, i_x_, g_x_, f_x_ = tf.split(x_cc, 7, 3)
+        c_new = f * c + i * g
 
-                i = tf.sigmoid(i_x + i_h + i_c)
-                f = tf.sigmoid(f_x + f_h + f_c + self._forget_bias)
-                g = tf.tanh(g_x + g_h + g_c)
+        c2m = self.conv_c2(c_new)
+        if self.layer_norm:
+            c2m = run_layer_norm(c2m, self.ln_c2)
 
-            c_new = f * c + i * g
+        i_c, g_c, f_c, o_c = torch.split(c2m, 4, 1)
 
-            c2m = tf.layers.conv2d(
-                c_new, self.num_hidden*4,
-                self.filter_size, 1, padding='same',
-                kernel_initializer=self.initializer,
-                name='c2m')
-            if self.layer_norm:
-                c2m = tensor_layer_norm(c2m, 'c2m')
+        if x is None:
+            ii = torch.sigmoid(i_c + i_m)
+            ff = torch.sigmoid(f_c + f_m + self._forget_bias)
+            gg = torch.tanh(g_c)
+        else:
+            ii = torch.sigmoid(i_c + i_x_ + i_m)
+            ff = torch.sigmoid(f_c + f_x_ + f_m + self._forget_bias)
+            gg = torch.tanh(g_c + g_x_)
 
-            i_c, g_c, f_c, o_c = tf.split(c2m, 4, 3)
+        m_new = ff * torch.tanh(m_m) + ii * gg
 
-            if x is None:
-                ii = tf.sigmoid(i_c + i_m)
-                ff = tf.sigmoid(f_c + f_m + self._forget_bias)
-                gg = tf.tanh(g_c)
-            else:
-                ii = tf.sigmoid(i_c + i_x_ + i_m)
-                ff = tf.sigmoid(f_c + f_x_ + f_m + self._forget_bias)
-                gg = tf.tanh(g_c + g_x_)
+        o_m = self.conv_o(m_new)
+        if self.layer_norm:
+            o_m = run_layer_norm(o_m, self.ln_o)
 
-            m_new = ff * tf.tanh(m_m) + ii * gg
+        if x is None:
+            o = torch.tanh(o_h + o_c + o_m)
+        else:
+            o = torch.tanh(o_x + o_h + o_c + o_m)
 
-            o_m = tf.layers.conv2d(
-                m_new, self.num_hidden,
-                self.filter_size, 1, padding='same',
-                kernel_initializer=self.initializer,
-                name='m_to_o')
-            if self.layer_norm:
-                o_m = tensor_layer_norm(o_m, 'm2o')
+        cell = torch.cat([c_new, m_new], -1)
+        cell = self.conv_h2(cell)
 
-            if x is None:
-                o = tf.tanh(o_h + o_c + o_m)
-            else:
-                o = tf.tanh(o_x + o_h + o_c + o_m)
+        h_new = o * torch.tanh(cell)
 
-            cell = tf.concat([c_new, m_new],-1)
-            cell = tf.layers.conv2d(cell, self.num_hidden, 1, 1,
-                                    padding='same', name='memory_reduce')
-
-            h_new = o * tf.tanh(cell)
-
-            return h_new, c_new, m_new
-
-
+        return h_new, c_new, m_new
